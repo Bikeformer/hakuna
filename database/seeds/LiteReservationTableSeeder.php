a78d9fa29c49236ce0512af35f300a50092d60b8
Valera <valera@MBP-Valera.local>
2017-08-21 11:16:37 +0300

    create seed

---
+++ database/seeds/LiteReservationTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LiteReservationTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('lite_reservations')->insert([
+            'seat_id' => 1,
+            'user_id' => 1,
+            'end_date' => \Carbon\Carbon::now()->addMinutes(60),
+        ]);
+
+    }
+}
